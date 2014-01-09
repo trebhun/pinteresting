de7cce31f6ec1380a0be468859b556ef87024b46
Tory Rebhun <trebhun@gmail.com>
2014-01-09 16:24:12 -0700

    Recently moved files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" #about_path created here
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
